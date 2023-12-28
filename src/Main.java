1ef79736589137e2677172d0d6be136a3add7aba
salma-4 <salmasobhy456@gmail.com>
2023-12-28 17:35:27 +0200

    edit lexical function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 import java.util.StringTokenizer;
 import  java.io.* ;
+import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
 * keyword
@@ -12,83 +18,91 @@ import  java.io.* ;
 * */
 public class Main {
 
-    //1
-    public static boolean keywordTokens(String tokens){
-        String keys[]={"int","long","float","double","boolean","String","if","else",
-                       "switch","case","continue","break","for","while","char","do","goto"
-                        ,"enum","private","public","void"
-        };
-        for(String key :keys){
-            if(tokens.equals(key)){
+    public static boolean keywordTokens(String tokens) {
+        String keys[] = { "int", "long", "float", "double", "boolean", "String", "if", "else", "switch", "case",
+                "continue", "break", "for", "while", "char", "do", "goto", "enum", "private", "public", "void" };
+        for (String key : keys) {
+            if (tokens.equals(key)) {
                 return true;
             }
         }
-      return  false;
+        return false;
     }
 
-    //2
-    public static boolean identifierTokens(String token){
-      if(token.matches("[a-zA-Z][a-zA-Z0-9_]*"))
-          return true;
-      else
-        return false;
+    public static boolean identifierTokens(String token) {
+        if (token.matches("[a-zA-Z][a-zA-Z0-9_]*"))
+            return true;
+        else
+            return false;
     }
-    //3
-    public static boolean operatorTokens(String token){
-        String[] operators={"+","-","*","/","++","--","==","!=","=","<",">","/","+=","-="};
-        for(String oper : operators){
-            if(token.equals(oper)){
+
+    public static boolean operatorTokens(String token) {
+        String[] operators = { "+", "-", "*", "/", "++", "--", "==", "!=", "=", "<", ">", "/", "+=", "-=" };
+        for (String oper : operators) {
+            if (token.equals(oper)) {
                 return true;
             }
         }
         return false;
     }
-      //4
-    public static boolean numericTokens(String token){
-        if(token.matches("\\d+(\\.\\d+)?"))
-            return true ;
+
+    public static boolean numericTokens(String token) {
+        if (token.matches("\\d+(\\.\\d+)?"))
+            return true;
         else
-          return false;
+            return false;
     }
-    //5
-    public static  boolean specialChars(String token){
-        String ch[]={",",";","(",")","{","}","[","]"};
-        for(String c: ch) {
+
+    public static boolean specialChars(String token) {
+        String ch[] = { ",", ";", "(", ")", "{", "}", "[", "]" };
+        for (String c : ch) {
             if (token.equals(c))
-                return  true;
+                return true;
         }
         return false;
     }
-    //6
-    public static boolean comment(String token){
+
+    public static boolean comment(String token) {
         return token.matches("//.*|/\\*(.|\\n)*?\\*/"); // single and double comments
     }
+
+    public static List<Pair<String, String>> analyzeLexical(String input) {
+        List<Pair<String, String>> sympolTable= new ArrayList<>();
+
+        StringTokenizer tokenizer = new StringTokenizer(input);
+
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+
+            if (keywordTokens(token)) {
+                sympolTable.add(new Pair<>("Keyword", token));
+            } else if (identifierTokens(token)) {
+                sympolTable.add(new Pair<>("Identifier", token));
+            } else if (operatorTokens(token)) {
+                sympolTable.add(new Pair<>("Operator", token));
+            } else if (numericTokens(token)) {
+                sympolTable.add(new Pair<>("Numeric Constant", token));
+            } else if (specialChars(token)) {
+                sympolTable.add(new Pair<>("Special Character", token));
+            } else if (comment(token)) {
+                sympolTable.add(new Pair<>("Comment", token));
+            } else {
+                sympolTable.add(new Pair<>("Invalid Token", token));
+            }
+        }
+
+        return sympolTable;
+    }
+
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new FileReader("D:\\Desktop\\Lexical analysis\\src\\read.txt"))) {
             String readLine;
 
             while ((readLine = reader.readLine()) != null) {
+                List<Pair<String, String>> lexicalResult = analyzeLexical(readLine);
 
-                StringTokenizer tokenizer = new StringTokenizer(readLine);
-
-                while (tokenizer.hasMoreTokens()) {
-                    String token = tokenizer.nextToken();
-
-                    if (keywordTokens(token)) {
-                        System.out.println("Keyword: " + token);
-                    } else if (identifierTokens(token)) {
-                        System.out.println("Identifier: " + token);
-                    } else if (operatorTokens(token)) {
-                        System.out.println("Operator: " + token);
-                    } else if (numericTokens(token)) {
-                        System.out.println("Numeric Constant: " + token);
-                    } else if (specialChars(token)) {
-                        System.out.println("Special Character: " + token);
-                    } else if (comment(token)) {
-                        System.out.println("Comment: " + token);
-                    } else {
-                        System.out.println("invalid token: " + token);
-                    }
+                for (Pair<String, String> pair : lexicalResult) {
+                    System.out.println(pair.getFirst() + ": " + pair.getSecond());
                 }
             }
         } catch (IOException e) {
